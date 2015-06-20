8d45bd820489f583835befe8a1bddee3016f5171
Kevin Lanzon <kevinlanzon@gmail.com>
2015-06-20 12:17:00 +0100

    adds show fact button

---
+++ FunFacts/Scratchpad.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Swift Recap
+
+var SomeVariable = "aVariable"
+let SomeConstant = 20
+
+SomeVariable = "anotherString"
+
+
+var fruitsArray = ["apples"]
+fruitsArray.append("bananas")
+
+fruitsArray
+
+//Optionals
+
+var optionalString: String? = "hello"
+optionalString!
+
+
+
+
